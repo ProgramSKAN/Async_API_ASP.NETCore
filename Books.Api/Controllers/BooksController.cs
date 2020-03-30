866549068960ab5b25608ad127cf5cc540e2f036
SandeepKandagatla <59524137+SandeepKandagatla@users.noreply.github.com>
2020-03-30 18:04:28 +0530

    Add files via upload

---
+++ Books.Api/Controllers/BooksController.cs
@@ -0,0 +1,41 @@
+﻿//using Microsoft.AspNetCore.Mvc;// :Controller>> used for MVC apps not APIs
+//after asp.net core 2.1 we use :ControllerBase with [ApiController] which enforces attribute routing >for APIS
+using Books.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Books.Api.Controllers
+{
+    [Route("api/books")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private IBooksRepository _booksRepository;
+
+        public BooksController(IBooksRepository booksRepository)
+        {
+            _booksRepository = booksRepository ?? throw new ArgumentNullException(nameof(booksRepository));
+        }
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetBooks()
+        {
+            var bookEntities = await _booksRepository.GetBooksAsync();
+            return Ok(bookEntities);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBook(Guid id)
+        {
+            var bookEntity = await _booksRepository.GetBookAsync(id);
+            if (bookEntity == null)
+            {
+                return NotFound();
+            }
+            return Ok(bookEntity);
+        }
+    }
+}
---
+++ Books.Api/Services/IBooksRepository.cs
@@ -0,0 +1,14 @@
+﻿using Books.Api.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Books.Api.Services
+{
+    public interface IBooksRepository
+    {
+        Task<IEnumerable<Book>> GetBooksAsync();
+        Task<Book> GetBookAsync(Guid id);
+    }
+}
